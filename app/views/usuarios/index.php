6378c8e7c20ca176f9c52f7482fe82dbcb264500
David Miranda <dm1236789@gmail.com>
2025-01-27 01:41:19 -0500

    refactor: evitar eliminacion usuario mismo
    Se hace validacion para que el usuario autenticado no se pueda eliminar a si mismo

--- app/views/usuarios/index.php
+++ app/views/usuarios/index.php
@@ -1,8 +1,10 @@
 <?php
+
 use App\Controllers\UsuariosController;
+
 $usuariosController = new UsuariosController();
 $usuarios = $usuariosController->get();
-$estado = function($estado) {
+$estado = function ($estado) {
     return $estado == 1000 ? 'checked' : '';
 };
 ?>
@@ -27,10 +29,10 @@ $estado = function($estado) {
                 <tbody class="table-group-divider">
                     <?php foreach ($usuarios as $usuario): ?>
                         <tr>
-                            <td><?=  $usuario['usu_nombre'] ?></td>
-                            <td><?=  $usuario['usu_cuenta'] ?></td>
-                            <td><?=  $usuario['usu_email'] ?></td>
-                            <td><input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" disabled <?=  $estado($usuario['usu_idEstado']) ?>></td>
+                            <td><?= $usuario['usu_nombre'] ?></td>
+                            <td><?= $usuario['usu_cuenta'] ?></td>
+                            <td><?= $usuario['usu_email'] ?></td>
+                            <td><input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" disabled <?= $estado($usuario['usu_idEstado']) ?>></td>
                             <td>
                                 <a class="py-2 px-4 btn btn-info btn-xs" href="/inventario/public/usuarios/editarusuario?usu_id=<?= $usuario['usu_id'] ?>">
                                     <svg class="btn-icon" width="24" height="24">
@@ -38,13 +40,17 @@ $estado = function($estado) {
                                     </svg>
                                 </a>
                             </td>
-                            <td class="d-flex">
-                            <a class="py-2 px-4 btn btn-danger btn-xs" href="/inventario/public/Usuarios/eliminarUsuario?usu_id=<?= $usuario['usu_id']?>">
-                                <svg class="btn-icon" width="24" height="24">
-                                    <use xlink:href="/inventario/public/icons/free.svg#cil-trash"></use>
-                                </svg> 
-                            </a>
-                        </td>
+                            <?php if ($_SESSION['username'] === $usuario['usu_nombre']): ?>
+                                <td></td>
+                            <?php else: ?>
+                                <td class="d-flex">
+                                    <a class="py-2 px-4 btn btn-danger btn-xs" href="/inventario/public/Usuarios/eliminarUsuario?usu_id=<?= $usuario['usu_id'] ?>">
+                                        <svg class="btn-icon" width="24" height="24">
+                                            <use xlink:href="/inventario/public/icons/free.svg#cil-trash"></use>
+                                        </svg>
+                                    </a>
+                                </td>
+                            <?php endif; ?>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
