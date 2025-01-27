faa900d1dac339c8efb9c2ea19452eb0b2e14f26
David Miranda <dm1236789@gmail.com>
2025-01-27 01:42:23 -0500

    feat: nuevo modulo de ayuda
    Se anexa panel de ayuda para los usuarios donde encuentran el acceso al proyecto y la documentacion pertinente

--- app/views/ayuda/index.php
+++ app/views/ayuda/index.php
@@ -1,5 +1,47 @@
-<div class="row">
-    <div class="col-md-12">
-        <h1>Bienvenido a Ayuda</h1>
+<br><br>
+<div class="card">
+    <div class="card-header">AYUDA</div>
+    <div class="card-body">
+        <section>
+            <p>
+                Este proyecto consiste en un completo sistema de gestión de inventarios diseñado para optimizar y simplificar la administración de productos, categorías y proveedores. Su objetivo principal es brindar una solución intuitiva, eficiente y escalable para pequeñas que deseen organizar y supervisar su inventario con mayor facilidad.
+            </p>
+            <p>Entre las funcionalidades destacadas, el sistema permite:</p>
+            <ul>
+                <li><strong>Gestión de Productos:</strong> Crear, editar y actualizar información sobre los productos almacenados.</li>
+                <li><strong>Manejo de Proveedores:</strong> Registrar y mantener información actualizada de los proveedores, mejorando la trazabilidad y el control de suministros.</li>
+            </ul>
+            <p>
+                El proyecto está desarrollado con PHP y MySQL, con un modelo MVC siguiendo buenas practicas de desarrollo.
+            </p>
+        </section>
+
+        <section>
+            <h2>Enlaces al Código Fuente</h2>
+            <p>
+                El código fuente del sistema está disponible en las siguientes plataformas para facilitar su acceso y colaboración:
+            </p>
+            <ul>
+                <li><a href="https://github.com/DMiranda2214/inventario" target="_blank">Repositorio en GitHub</a></li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Documentación del Proyecto</h2>
+            <p>
+                Toda la información necesaria para comprender, instalar y utilizar el sistema se encuentra documentada de manera exhaustiva. Los recursos disponibles incluyen:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://drive.google.com/file/d/1qZr4tNpan_7xiYtGOyKBoctzyKn8iOs_/view?usp=drive_link" target="_blank">Documentación de instalacion (PDF)</a>: Esta documentación abarca todo el proceso de instalacion de XAMPP, Composer, Notepad++ y configuracion del motor de base datos, asi como la configuracion del archivo de variables de entorno y instalacion de dependencias.
+                </li>
+                <li>
+                    <a href="#" target="_blank">Manual de Usuario (PDF)</a>: Una guía práctica destinada a los usuarios finales, con instrucciones detalladas para navegar y utilizar todas las funciones del sistema de inventario.
+                </li>
+            </ul>
+            <p>
+                Con estos recursos, tanto desarrolladores como usuarios podrán sacar el máximo provecho del sistema. Si tienes preguntas o sugerencias, no dudes en contribuir o contactar a los desarrolladores a través de los repositorios.
+            </p>
+        </section>
     </div>
 </div>
